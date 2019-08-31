2816016391e487c5b91d1c17ed68ea0e548b505d
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-31 17:30:00 +0530

    update log

---
+++ routes/functions/tamilmvFunction.js
@@ -0,0 +1,42 @@
+// Load Item model
+const Item = require("../../models/Item");
+const tamilmv = require("../sites/tamilmv");
+const sendPushMsg = require("./pushbullet");
+
+const tamilmvFunction = () => {
+  console.log("Now running psaripsFunction");
+  tamilmv()
+    .then(links => {
+      console.log(links);
+      // var namesArray = Object.keys(links);
+      // namesArray.forEach((name, index) => {
+      //   const newItem = { name: name, link: links[name] };
+      //   Item.findOne({ name: name }).then(item => {
+      //     if (item) {
+      //       //throw an error
+      //       console.log("PSARips error: Link name exists!");
+      //     } else {
+      //       //create new item
+      //       new Item(newItem)
+      //         .save()
+      //         .then(item => {
+      //           console.log(item);
+      //           sendPushMsg(item.name, item.link)
+      //             .then(res => {
+      //               console.log(res);
+      //             })
+      //             .catch(err => {
+      //               console.log(err);
+      //             });
+      //         })
+      //         .catch(err => console.log(err));
+      //     }
+      //   });
+      // });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+module.exports = tamilmvFunction;
---
+++ routes/sites/tamilmv.js
@@ -0,0 +1,40 @@
+const cheerio = require("cheerio");
+const request = require("request");
+
+var url;
+var options = {};
+
+const tamilmv = () => {
+  return new Promise((resolve, reject) => {
+    url = `https://www.tamilmv.bid/index.php`;
+    url = encodeURI(url);
+
+    options = {
+      method: "GET",
+      url: url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
+      }
+    };
+
+    try {
+      request(options, function(err, response, body) {
+        if (err) reject(err);
+
+        const $ = cheerio.load(body);
+
+        var title = {};
+        var update = [];
+
+        update = $("div.ipsWidget_inner").find("strong");
+
+        resolve(update);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = tamilmv;
