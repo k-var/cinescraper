dd82b91547ea26df7260b5c6e847597a578c89ad
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-09-01 13:10:01 +0530

    update log

--- routes/functions/tamilmvFunction.js
+++ routes/functions/tamilmvFunction.js
@@ -8,32 +8,31 @@ const tamilmvFunction = () => {
   //tamil web
   tamilWeb()
     .then(links => {
-      console.log(links);
-      // var namesArray = Object.keys(links);
-      // namesArray.forEach((name, index) => {
-      //   const newItem = { name: name, link: links[name] };
-      //   Item.findOne({ name: name }).then(item => {
-      //     if (item) {
-      //       //throw an error
-      //       console.log("PSARips error: Link name exists!");
-      //     } else {
-      //       //create new item
-      //       new Item(newItem)
-      //         .save()
-      //         .then(item => {
-      //           console.log(item);
-      //           sendPushMsg(item.name, item.link)
-      //             .then(res => {
-      //               console.log(res);
-      //             })
-      //             .catch(err => {
-      //               console.log(err);
-      //             });
-      //         })
-      //         .catch(err => console.log(err));
-      //     }
-      //   });
-      // });
+      var namesArray = Object.keys(links);
+      namesArray.forEach((name, index) => {
+        const newItem = { name: name, link: links[name] };
+        Item.findOne({ name: name }).then(item => {
+          if (item) {
+            //throw an error
+            console.log("Tamilmv error: Link name exists!");
+          } else {
+            //create new item
+            new Item(newItem)
+              .save()
+              .then(item => {
+                console.log(item);
+                sendPushMsg(item.name, item.link)
+                  .then(res => {
+                    console.log(res);
+                  })
+                  .catch(err => {
+                    console.log(err);
+                  });
+              })
+              .catch(err => console.log(err));
+          }
+        });
+      });
     })
     .catch(err => {
       console.log(err);
